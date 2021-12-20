2aafc314680c05c08f3c65e2f4a31e5a02f4a609
ChaimW25 <74601548+ChaimW25@users.noreply.github.com>
2021-12-20 20:09:35 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,8 +3,23 @@
 #include <string.h>
 
 int main(){
-    fgets(word,WORD,stdin);
-    fgets(txt,TXT,stdin);
+//    fgets(word,WORD,stdin);
+//    fgets(txt,TXT,stdin);
+//char tmp1;
+//char tmp2;
+for(int i=0; i<WORD; i++){
+    scanf("%c",&word[i]);
+    if(word[i]=='\n'||word[i]=='\t'||word[i]==32){
+        break;
+    }
+}
+    for(int i=0; i<TXT; i++){
+        scanf("%c",&txt[i]);
+        if(txt[i]=='~'){
+            break;
+        }
+    }
+
     gematria();
     atbash();
     anagram();
